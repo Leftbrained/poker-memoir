385d49563eff92c2b84b0e09af06b89de8a7f1df
Chris Testroet <chris@leftbrained.org>
2021-06-06 16:51:43 -0700

    Created SessionCreateActivity, open it from MainActivity

--- app/src/main/java/org/leftbrained/pokermemoir/MainActivity.kt
+++ app/src/main/java/org/leftbrained/pokermemoir/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.leftbrained.pokermemoir
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val EXTRA_START_SESSION = "org.leftbrained.pokermemoir.START_SESSION"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createSession(view: View) {
+        val startSession = true
+        val intent = Intent(this, SessionCreateActivity::class.java).apply {
+            putExtra(EXTRA_START_SESSION, startSession)
+        }
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/org/leftbrained/pokermemoir/SessionCreateActivity.kt
@@ -0,0 +1,11 @@
+package org.leftbrained.pokermemoir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SessionCreateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_session_create)
+    }
+}
